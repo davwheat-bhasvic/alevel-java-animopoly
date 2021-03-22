7f6130127b812f96ae48124471ed768c057e471b
David Wheatley <david.wheatley20@students.bhasvic.ac.uk>
2021-03-22 14:52:00 +0000

    Fix docs generation

--- generate-docs.sh
+++ generate-docs.sh
@@ -4,6 +4,6 @@
 mkdir -p javadoc
 
 cd javadoc
-javadoc --enable-preview -notimestamp ../src/src/dev/davwheat/*.java ../src/src/dev/davwheat/enums/*.java ../src/src/dev/davwheat/exceptions/*.java
+javadoc --release 12 --enable-preview -notimestamp ../src/src/dev/davwheat/*.java ../src/src/dev/davwheat/enums/*.java ../src/src/dev/davwheat/exceptions/*.java
 
 cd ..
