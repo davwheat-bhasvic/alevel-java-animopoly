274f0fab426bcd83aa8a83c80a0e3f8c888877e3
David Wheatley <david.wheatley20@students.bhasvic.ac.uk>
2021-03-24 10:14:46 +0000

    Add new turn code

--- src/src/dev/davwheat/Player.java
+++ src/src/dev/davwheat/Player.java
@@ -3,9 +3,13 @@ package dev.davwheat;
 import dev.davwheat.enums.BoardSpaceType;
 import dev.davwheat.enums.Color;
 import dev.davwheat.exceptions.AnimalAlreadyOwnedException;
+import dev.davwheat.exceptions.AnimalNotOwnedException;
+import dev.davwheat.exceptions.AnimalUpgradeNotAllowedException;
 import dev.davwheat.exceptions.InsufficientBalanceException;
 
-import java.util.Scanner;
+import javax.naming.NoPermissionException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A Player who is engaged in the current game.
@@ -208,10 +212,18 @@ public class Player {
         if (justPassedGo) {
             if (currentSpace.type == BoardSpaceType.START) {
                 System.out.println("You landed on Start! Collect £1000");
-                this.adjustBankBalance(1000);
+                try {
+                    this.adjustBankBalance(1000);
+                } catch (InsufficientBalanceException e) {
+                    e.printStackTrace();
+                }
             } else {
                 System.out.println("You just passed Start! Collect £500");
-                this.adjustBankBalance(500);
+                try {
+                    this.adjustBankBalance(500);
+                } catch (InsufficientBalanceException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
@@ -233,12 +245,14 @@ public class Player {
                 if (shouldBuy) {
                     try {
                         animalSpace.purchase(this);
-                        System.out.printf("%sYou now own %s! New balance: £%.2f%s", Color.BLUE_BOLD, animalSpace.displayName, this.currentBankBalance, Color.RESET);
+                        System.out.printf("%sYou now own %s! New balance: £%.2f%s\n", Color.BLUE_BOLD, animalSpace.displayName, this.currentBankBalance, Color.RESET);
                     } catch (final AnimalAlreadyOwnedException e) {
                         e.printStackTrace();
+                    } catch (final InsufficientBalanceException e) {
+                        System.out.println("You can't afford to pay for this Animal.");
                     }
                 }
-            } else if (owner.equals(this)) {
+            } else if (animalSpace.isOwnedBy(this)) {
                 // This is their own property!
                 System.out.printf("%sYou own this animal, so stopping here is free.%s\n", Color.BLUE_BOLD, Color.RESET);
             } else {
@@ -248,17 +262,110 @@ public class Player {
                 System.out.println("Press ENTER to continue.");
                 ioHelper.pressEnterToContinue();
 
-                animalSpace.payForStop(this);
+                try {
+                    animalSpace.payForStop(this);
+                } catch (InsufficientBalanceException e) {
+                    System.out.println("You can't afford to pay for this stop. You're out!");
+                    this.currentBankBalance = -1;
+                    return;
+                }
                 System.out.printf("Your new balance is %s£%.2f%s.\n", Color.GREEN_BOLD_BRIGHT, this.currentBankBalance, Color.RESET);
             }
         } else if (currentSpace.type == BoardSpaceType.MISS_NEXT_TURN) {
             System.out.println("You landed on \"Miss next turn\".");
         }
 
+        if (dice.isDouble()) {
+            final char chance = ioHelper.readChar("You rolled a double! Would you like to pick up a Chance card? (Y/N)", "Please choose either Y for yes or N for no.", IOHelper.YesNoCharValidator);
+
+            if (String.valueOf(chance).equalsIgnoreCase("y")) {
+                // Pick up chance card
+                final Card card = this.gameInstance.cardDeck.takeCard();
+                card.printCard();
+                System.out.println("\nPress ENTER to continue.");
+                ioHelper.pressEnterToContinue();
+
+                // Take the action
+                try {
+                    card.takeAction(this);
+                } catch (InsufficientBalanceException e) {
+                    System.out.println("You can't afford to pay what this card requires. You're out!");
+                    this.currentBankBalance = -1;
+                    return;
+                }
+
+                System.out.printf("Your new balance is %s£%.2f%s.\n", Color.GREEN_BOLD_BRIGHT, this.currentBankBalance, Color.RESET);
+            }
+        }
+
+        int option = 0;
+
+        while (option != 2) {
+            System.out.printf("\n%sPlayer turn menu%s\n", Color.BLUE_BOLD_BRIGHT, Color.RESET);
+            System.out.println("\n1. Upgrade Animal");
+            System.out.println("2. End turn");
+            option = ioHelper.readInteger("Choose an option: ", "Please choose either 1 or 2.", i -> i >= 1 && i <= 2);
+
+            switch (option) {
+                case 1 -> this.upgradeAnimal(ioHelper);
+            }
+        }
+
         // This should never error... hopefully...
         try {
             this.gameInstance.endTurn(this);
         } catch (final Exception ignored) {
+        }
+    }
+
+    private void upgradeAnimal(final IOHelper ioHelper) {
+        while (true) {
+            System.out.printf("\n%sUpgrade an animal%s\n", Color.BLUE_BOLD_BRIGHT, Color.RESET);
+            final List<Animal> myAnimals = this.gameInstance.gameBoardInstance.getOwnedAnimals(this);
+
+            final AtomicInteger i = new AtomicInteger(1);
+            myAnimals.forEach(a -> {
+                final int num = i.getAndIncrement();
+                System.out.printf("%d. %s (Current: L%d)\n", num, a.displayName, a.getCurrentLevel().value);
+            });
+
+            final int backNum = i.getAndIncrement();
+            System.out.printf("%d. Back\n", backNum);
+
+            final int option = ioHelper.readInteger(
+                    "Choose an option: ",
+                    String.format("Please choose a value between 1 and %d.", i.get()),
+                    x -> x >= 1 && x <= i.get()
+            );
+
+            // Back
+            if (option == backNum) {
+                return;
+            }
+
+            final Animal animal = myAnimals.get(option - 1);
+
+            if (!animal.isUpgradable(this)) {
+                System.out.printf("\"%s\" is not upgradable.", animal.displayName);
+                continue;
+            }
+
+            System.out.printf("Upgrading \"%s\" will cost £%.2f\n", animal.displayName, animal.upgradeCost);
+            final boolean upgrade = String.valueOf(ioHelper.readChar("Would you like to upgrade? (Y/N)", "Please choose Y for yes, or N for no.", IOHelper.YesNoCharValidator))
+                    .equalsIgnoreCase("y");
+
+            if (upgrade) {
+                try {
+                    animal.upgrade(this);
+                    System.out.printf("\"%s\" has been upgraded to level %d!\n", animal.displayName, animal.getCurrentLevel().value);
+                    System.out.printf("Your new balance is %s£%.2f%s.\n", Color.GREEN_BOLD_BRIGHT, this.currentBankBalance, Color.RESET);
+                } catch (AnimalNotOwnedException | NoPermissionException | AnimalUpgradeNotAllowedException e) {
+                    e.printStackTrace();
+                } catch (InsufficientBalanceException e) {
+                    System.out.printf("%sYou don't have enough money to upgrade this.%s\n", Color.RED_BOLD_BRIGHT, Color.RESET);
+                    System.out.printf("Your balance is %s£%.2f%s.\n", Color.GREEN_BOLD_BRIGHT, this.currentBankBalance, Color.RESET);
+                }
+            }
         }
     }
 }
